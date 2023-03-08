3d3aaca83b0b2a225dafbcddae42b4182bf128db
KianShah10385 <108907854+KianShah10385@users.noreply.github.com>
2023-03-07 19:05:37 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+NoseX = 0;
+NoseY = 0;
+
 function preload() {
 }
 
@@ -5,11 +8,31 @@ function setup()
 {
     canvas = createCanvas(300,300);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initialized');
 }
 
 function draw()
 {
+    image(video, 0, 0, 300, 300);
 }
 function take_snapshot() {
-save('class123.png');
+save('Class123-125.png');
+}
+function gotPoses(results)
+{
+    if(results.length>0)
+    {
+        console.log("results")
+        NoseX = results[0].pose.Nose.x;
+        NoseY = results[0].pose.Nose.y;
+    }
 }
